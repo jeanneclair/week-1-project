d485e9b0fa9d7254b504b3246bb2d1716cb5cacd
Jeanne Clair <jeanne.m.clair@gmail.com>
2018-05-18 10:41:40 -0400

    initial commit

---
+++ src/FortuneTeller.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FortuneTeller {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What is your first name?");
+		String firstName = input.nextLine();
+		
+		System.out.println("What is your last name?");
+		String lastName = input.nextLine();
+		
+	}
+
+}
